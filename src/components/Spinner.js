3bc3e89c700cbfb9f08858766b7857c564add503
Brandon Barksdale <bbark93@gmail.com>
2023-06-12 23:02:25 -0400

    finished step 2 and 3 in Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -51,10 +51,10 @@ const [spinnerOn, setSpinnerOn] = useState(true);
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn ? 'Hide' : 'Show'} Spinner {/* STEP 3 */}
       </button>
     </div>
   );
